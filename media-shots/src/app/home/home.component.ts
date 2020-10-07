b1cccfe08c3eef733bc3e41cc19328e89797d0be
Reza Fitriaman <rezafitriaman@gmail.com>
2020-10-07 12:17:29 +0200

    change text

--- media-shots/src/app/home/home.component.ts
+++ media-shots/src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent {
     'Mediashots werkt met jou samen vanaf idee tot het eind product';
   @Output() urlVideo =
     'https://www.youtube.com/embed/HCDVN7DCzYE?playlist=HCDVN7DCzYE&controls=0&showinfo=0&loop=1&autoplay=1&mute=1';
-  @Output() title = 'Samenwerking';
+  @Output() title = 'Samenwerking ';
   @Output() carouselItems: Array<Carousel> = [
     new Carousel('../assets/img/logos-smaller.png'),
     new Carousel('../assets/img/logos-smaller1.png'),
